2256c0b0fe17886c03665dadc3400413b198ffb8
zhoozhoo <zhoozhoo@yahoo.com>
2014-03-12 16:13:04 -0400

    Renamed class.

--- spring-wicket/src/main/java/ca/zhoozhoo/examples/springwicket/ui/WicketApplication.java
+++ spring-wicket/src/main/java/ca/zhoozhoo/examples/springwicket/ui/WicketApplication.java
@@ -1,11 +1,15 @@
 package ca.zhoozhoo.examples.springwicket.ui;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
 
 import ca.zhoozhoo.examples.springwicket.ui.pages.HelloWorld;
 
-public class ExampleWebApplication extends WebApplication {
+public class WicketApplication extends WebApplication {
+
+	static final Logger logger = LogManager.getLogger(WicketApplication.class.getName());
 
 	@Override
 	public Class<? extends Page> getHomePage() {
